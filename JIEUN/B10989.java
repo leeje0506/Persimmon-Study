7a772427bc2439b7deaca5403382c5dd03e46924
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-05-28 17:23:45 +0900

    [BOJ] 수 정렬하기 3 / 브론즈 1

---
+++ JIEUN/B10989.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+/*
+입력 : N, N개의 수
+출력 : N줄로 출력
+조건 : 오름차순, N(1<=N<=10,000,000), N개의 숫자들 : 이 수는 10,000보다 작거나 같은 자연수
+풀이 예상 :
+어쩐지 퀵 정렬로 하면 시간 초과가 날 것만 같다. (최악일 때 N^2이기 때문에..)
+찾아보니 데이터의 범위가 제한된 경우 "카운팅 정렬(계수 정렬):항상 O(N+K)의 시간 복잡도"이 최적의 방법일 수 있단다.
+ */
+public class B10989 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int [] cnt = new int [10001];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            cnt[num]++;
+        }
+
+        for (int i = 0; i < cnt.length; i++) {
+            while(cnt[i] > 0){
+                sb.append(i).append("\n");
+                //순회하면서 카운트 줄여주기
+                cnt[i]--;
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+}
